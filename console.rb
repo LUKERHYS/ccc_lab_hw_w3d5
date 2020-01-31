6c883c9934472606037a6dcb778fc6634fb32eb9
lukerhys <hello@lukemelvin.com>
2020-01-31 17:02:43 +0000

    Initial commit

---
+++ console.rb
@@ -0,0 +1,12 @@
+require_relative('models/customers')
+require_relative('models/film')
+require_relative('models/ticket')
+
+require('pry')
+
+
+
+
+
+binding.pry
+nil
---
+++ db/codeclan_cinema.sql
@@ -0,0 +1,21 @@
+DROP TABLE tickets
+DROP TABLE cusomters
+DROP TABLE films
+
+CREATE TABLE tickets (
+  id SERIAL PRIMARY KEY,
+  customer_id INT REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT REFERENCES films(id) ON DELETE CASCADE
+  )
+
+CREATE TABLE customers (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  funds INT
+  )
+
+  CREATE TABLE films (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(255),
+    price INT
+    )
